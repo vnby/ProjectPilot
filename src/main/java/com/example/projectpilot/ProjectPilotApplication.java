607d9e0dc9d604954b7d29752060bd0225bcb2e3
Muhammad Alvin Abyan <alvin@Alvins-MacBook-Pro.local>
2021-07-11 16:27:35 +0700

    First commit

---
+++ src/main/java/com/example/projectpilot/ProjectPilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectpilot/ProjectPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
