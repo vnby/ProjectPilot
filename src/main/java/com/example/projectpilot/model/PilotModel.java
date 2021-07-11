e8fc5ffa0239657bcd46f8e45d3313c401638288
Alvin Abyan <malvinabyan@gmail.com>
2021-07-11 16:42:57 +0700

    Add new model + its constructor, setter, and getter

---
+++ src/main/java/com/example/projectpilot/model/PilotModel.java
@@ -0,0 +1,43 @@
+package com.example.projectpilot.model;
+
+public class PilotModel {
+    private String id;
+    private String licenseNumber;
+    private String name;
+    private Integer flyHour;
+
+    public PilotModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getFlyHour() {
+        return flyHour;
+    }
+
+    public void setFlyHour(Integer flyHour) {
+        this.flyHour = flyHour;
+    }
+}
